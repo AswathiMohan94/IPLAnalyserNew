d5d209d062034387a9ceb5fd4f931f78114f95e6
AswathiMohan94 <aswathimohantly@gmail.com>
2020-04-24 20:46:36 +0530

    [Aswathi] refactored All the test cases for implementing mockito

--- src/test/java/IndianPremierLeageTest/IPLAnalyserTest.java
+++ src/test/java/IndianPremierLeageTest/IPLAnalyserTest.java
@@ -20,21 +20,21 @@ public class IPLAnalyserTest {
 
     private static final String IPL_CSV = "./src/test/resources/IPL2019FactsheetMostRuns.csv";
     private static final String IPL_CSV_WICKETS = "./src/test/resources/IPL2019FactsheetMostWkts.csv";
-    private static final String MOCKITO_TEST_CSV="./src/test/resources/csvToTest.csv";
-    private static final String MOCKITO_WKT_CSV="./src/test/resources/csvToTestWkts.csv";
+    private static final String MOCKITO_TEST_CSV = "./src/test/resources/csvToTest.csv";
+    private static final String MOCKITO_WKT_CSV = "./src/test/resources/csvToTestWkts.csv";
 
 
     IPLAnalyser iplAnalyser = null;
 
     @Mock
-        IPLAnalyser iplAnalyserMock;
+    IPLAnalyser iplAnalyserMock;
 
 
     @Rule
     public MockitoRule mockitoRule = MockitoJUnit.rule();
 
     Map<String, Player> battingMap;
-    Map<String,Player> bowlingMap;
+    Map<String, Player> bowlingMap;
 
 
     @Before
@@ -43,12 +43,13 @@ public class IPLAnalyserTest {
         this.iplAnalyserMock = mock(IPLAnalyser.class);
 
     }
+
     @Test
-    public void TopBattingAveragesOfCricketers()  {
+    public void TopBattingAveragesOfCricketers() {
         try {
             iplAnalyser.loadIPLdata(MOCKITO_TEST_CSV);
             when(iplAnalyserMock.getAverageWiseSortedData()).thenReturn("xyz");
-            Assert.assertEquals("xyz",iplAnalyserMock.getAverageWiseSortedData());
+            Assert.assertEquals("xyz", iplAnalyserMock.getAverageWiseSortedData());
             verify(iplAnalyserMock).getAverageWiseSortedData();
 
         } catch (IPLAnalyserException e) {
@@ -62,64 +63,76 @@ public class IPLAnalyserTest {
         try {
             iplAnalyser.loadIPLdata(MOCKITO_TEST_CSV);
             when(iplAnalyserMock.getStrikingRates()).thenReturn("uvw");
-            Assert.assertEquals("uvw",iplAnalyserMock.getStrikingRates());
+            Assert.assertEquals("uvw", iplAnalyserMock.getStrikingRates());
             verify(iplAnalyserMock).getStrikingRates();
         } catch (IPLAnalyserException e) {
             e.printStackTrace();
         }
     }
+
     @Test
-    public void Max_6s_4s()  {              //mockito 3
+    public void Max_6s_4s() {              //mockito 3
 
         when(iplAnalyserMock.Max_six_four(MOCKITO_TEST_CSV)).thenReturn("xyz");
-        Assert.assertEquals("xyz",iplAnalyserMock.Max_six_four(MOCKITO_TEST_CSV));
+        Assert.assertEquals("xyz", iplAnalyserMock.Max_six_four(MOCKITO_TEST_CSV));
         verify(iplAnalyserMock).Max_six_four(MOCKITO_TEST_CSV);
     }
+
     @Test
-    public void BestStrikeRate_with_6s()  {     //mockito 4
+    public void BestStrikeRate_with_6s() {     //mockito 4
         when(iplAnalyserMock.BestStrikeRate_Six(MOCKITO_TEST_CSV)).thenReturn("uvw");
-        Assert.assertEquals("uvw",iplAnalyserMock.BestStrikeRate_Six(MOCKITO_TEST_CSV));
+        Assert.assertEquals("uvw", iplAnalyserMock.BestStrikeRate_Six(MOCKITO_TEST_CSV));
         verify(iplAnalyserMock).BestStrikeRate_Six(MOCKITO_TEST_CSV);
     }
+
     @Test
 
-    public void BestStrikeRate_with_4s()  { //mockito 5
+    public void BestStrikeRate_with_4s() { //mockito 5
         when(iplAnalyserMock.BestStrikeRate_four(MOCKITO_TEST_CSV)).thenReturn("uvw");
-        Assert.assertEquals("uvw",iplAnalyserMock.BestStrikeRate_four(MOCKITO_TEST_CSV));
+        Assert.assertEquals("uvw", iplAnalyserMock.BestStrikeRate_four(MOCKITO_TEST_CSV));
         verify(iplAnalyserMock).BestStrikeRate_four(MOCKITO_TEST_CSV);
     }
+
     @Test
     public void BestAvg_StrikeRate() {      //mockito 6
         when(iplAnalyserMock.BestAvg_StrikingRate(MOCKITO_TEST_CSV)).thenReturn("xyz");
         Assert.assertEquals("xyz", iplAnalyserMock.BestAvg_StrikingRate(MOCKITO_TEST_CSV));
         verify(iplAnalyserMock).BestAvg_StrikingRate(MOCKITO_TEST_CSV);
     }
+
     @Test
-    public void Max_Runs_BestAvg()  { //mockito 7
+    public void Max_Runs_BestAvg() { //mockito 7
         when(iplAnalyserMock.Max_Runs_BestAvg(MOCKITO_TEST_CSV)).thenReturn("xyz");
         Assert.assertEquals("xyz", iplAnalyserMock.Max_Runs_BestAvg(MOCKITO_TEST_CSV));
         verify(iplAnalyserMock).Max_Runs_BestAvg(MOCKITO_TEST_CSV);
     }
+
     @Test
-    public void Bowling_Best_Avg()  {
+    public void Bowling_Best_Avg() {
         try {
             iplAnalyser.loadBowlingdata(MOCKITO_WKT_CSV);  //mockito 8
             when(iplAnalyserMock.getMaxBowling_Avg()).thenReturn("pqr");
-            Assert.assertEquals("pqr",iplAnalyserMock.getMaxBowling_Avg());
+            Assert.assertEquals("pqr", iplAnalyserMock.getMaxBowling_Avg());
             verify(iplAnalyserMock).getMaxBowling_Avg();
-        } catch (IPLAnalyserException e) { e.printStackTrace();}
+        } catch (IPLAnalyserException e) {
+            e.printStackTrace();
+        }
     }
+
     @Test
-    public void TopStrikingRates_Bowlers()  {
+    public void TopStrikingRates_Bowlers() {
         try {
             iplAnalyser.loadBowlingdata(MOCKITO_WKT_CSV);    //mockito 9
             when(iplAnalyserMock.getStrikingRates_bowlers()).thenReturn("uvw");
             Assert.assertEquals("uvw", iplAnalyserMock.getStrikingRates_bowlers());
             verify(iplAnalyserMock).getStrikingRates_bowlers();
-        } catch (IPLAnalyserException e) { e.printStackTrace(); }
+        } catch (IPLAnalyserException e) {
+            e.printStackTrace();
+        }
     }
+
     @Test
-    public void BestEconomyRates_Bowlers()  {
+    public void BestEconomyRates_Bowlers() {
         try {
             iplAnalyser.loadBowlingdata(MOCKITO_WKT_CSV);    //mockito 10
             when(iplAnalyserMock.BestEconomyRates_Bowlers()).thenReturn("uvw");
@@ -129,20 +142,24 @@ public class IPLAnalyserTest {
 
             e.printStackTrace();
         }
-    }  @Test
-    public void BestStrikingRates_4w_Bowlers()  {
+    }
+
+    @Test
+    public void BestStrikingRates_4w_Bowlers() {
         when(iplAnalyserMock.BestStrikingRates_5w(MOCKITO_WKT_CSV)).thenReturn("abc");
         Assert.assertEquals("abc", iplAnalyserMock.BestStrikingRates_5w(MOCKITO_WKT_CSV));
         verify(iplAnalyserMock).BestStrikingRates_5w(MOCKITO_WKT_CSV);
     }
+
     @Test
-    public void BestAverages_with_StrikingRates_Bowlers()  {
+    public void BestAverages_with_StrikingRates_Bowlers() {
         when(iplAnalyserMock.BowlingAverages_StrikingRates(MOCKITO_WKT_CSV)).thenReturn("abc");
         Assert.assertEquals("abc", iplAnalyserMock.BowlingAverages_StrikingRates(MOCKITO_WKT_CSV));
         verify(iplAnalyserMock).BowlingAverages_StrikingRates(MOCKITO_WKT_CSV);
     }
+
     @Test
-    public void Wickets_BestAvg_Bowlers()  {
+    public void Wickets_BestAvg_Bowlers() {
         when(iplAnalyserMock.Wickets_BestAvg(MOCKITO_WKT_CSV)).thenReturn("abc");
         Assert.assertEquals("abc", iplAnalyserMock.Wickets_BestAvg(MOCKITO_WKT_CSV));
         verify(iplAnalyserMock).Wickets_BestAvg(MOCKITO_WKT_CSV);
@@ -154,16 +171,10 @@ public class IPLAnalyserTest {
         Assert.assertEquals("abc", iplAnalyserMock.Best_Batting_Bowling_Avg(MOCKITO_WKT_CSV, MOCKITO_TEST_CSV));
         verify(iplAnalyserMock).Best_Batting_Bowling_Avg(MOCKITO_WKT_CSV, MOCKITO_TEST_CSV);
     }
-
     @Test
     public void Best_Runs_wickets() {
-
-
-        String Data = iplAnalyser.Best_Runs_wickets(IPL_CSV_WICKETS,IPL_CSV);
-        Assert.assertEquals("David Warner", Data);
-    }
-
-
-    private class getAverageWiseSortedData {
+        when(iplAnalyserMock.Best_Runs_wickets(MOCKITO_WKT_CSV, MOCKITO_TEST_CSV)).thenReturn("xyz");
+        Assert.assertEquals("xyz", iplAnalyserMock.Best_Runs_wickets(MOCKITO_WKT_CSV, MOCKITO_TEST_CSV));
+        verify(iplAnalyserMock).Best_Runs_wickets(MOCKITO_WKT_CSV, MOCKITO_TEST_CSV);
     }
 }
